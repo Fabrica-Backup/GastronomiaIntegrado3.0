5d86c679a972db608a6afd88aa7659b80a5476ef
Lucas Mueller <lucas.mueller@opet.edu.br>
2017-10-26 17:29:35 -0200

    Commit 26/10
    Receita Get Funcionando 100%

--- main_app_receitas/static/js/extra/nova-receita/ajaxReceitas2.js
+++ main_app_receitas/static/js/extra/nova-receita/ajaxReceitas2.js
@@ -2,16 +2,21 @@
 // armazena os objetos json de ingrediente e unidade para ser usado em outros locais
 window.jsonReceita;
 window.jsonClassificacao;
+window.jsonCategoria;
 
 // get da tabela de ingredientes
-if (typeof jsonReceita === 'undefined' || typeof jsonObjectClassificacao === 'undefined') {
+if (typeof jsonReceita === 'undefined' || typeof jsonObjectClassificacao === 'undefined' || typeof jsonObjectCategoria === 'undefined') {
     $.getJSON('http://localhost:8000/api/receita/list', function (jsonObjectReceita) {
         jsonReceita = jsonObjectReceita;
 
         // get da tabela de unidades
         $.getJSON('http://localhost:8000/api/classificacao/list', function (jsonObjectClassificacao) {
             jsonClassificacao = jsonObjectClassificacao;
-            mostraReceitas();
+
+            $.getJSON('http://localhost:8000/api/categoria/list', function (jsonObjectCategoria) {
+                jsonCategoria = jsonObjectCategoria;
+                 mostraReceitas();
+            })
         })
     })
 } else {
@@ -23,21 +28,26 @@ function mostraReceitas() {
     $.each(jsonReceita, function (indexReceita, valReceita) {
         // roda a lista de unidades
         $.each(jsonClassificacao, function (indexClassificacao, valClassificacao) {
-            // compara as id de unidade das tabelas ingredientes e unidade e armazena a key 'descricao' da tabela unidade na variavel unidade
-            if (valClassificacao.id_classificacao == valReceita.id_classificacao) {
-                var row = $('<tr class="id-receita" data-id="' + valReceita.id_receita + '"></tr>');
-                var buttonEdit = '<td><button type="button" class="btn btn-md" data-toggle="modal" data-target="#editReceita"><i class="fa fa-edit"></i></button></td>';
-                var buttonDelete = '<td><button type="button" id="buttonDeletar" class="btn btn-danger btn-md excluir" ><i class="fa fa-trash-o"></i></button></td>';
-                var buttonView = '<td><button type="button" class="btn btn-default btn-md" data-toggle="modal" data-target="#visualizar"><i class="fa fa-eye" aria-hidden="true"></i></span> Visualizar Receita </button></td>';
 
-                $("#tableReceitas").append(row); //this will append tr element to table... keep its reference for a while since we will add cels into it
-                row.append($("<td>" + valReceita.nome_receita + "</td>"));
-                row.append($("<td>" + valClassificacao.descricao_classificacao + "</td>"));
-                // row.append($("<td>" + valCategoria.descricao_categoria + "</td>"))
-                row.append(buttonEdit);
-                row.append(buttonDelete);
-                row.append(buttonView);
-           }
+            $.each(jsonCategoria, function (indexCategoria, valCategoria) {
+                // compara as id de unidade das tabelas ingredientes e unidade e armazena a key 'descricao' da tabela unidade na variavel unidade
+                if (valReceita.id_classificacao == valClassificacao.id_classificacao) {
+                    if (valReceita.id_categoria == valCategoria.id_categoria) {
+                        var row = $('<tr class="id-receita" data-id="' + valReceita.id_receita + '"></tr>');
+                        var buttonEdit = '<td><button type="button" class="btn btn-md" data-toggle="modal" data-target="#editReceita"><i class="fa fa-edit"></i></button></td>';
+                        var buttonDelete = '<td><button type="button" id="buttonDeletar" class="btn btn-danger btn-md excluir" ><i class="fa fa-trash-o"></i></button></td>';
+                        var buttonView = '<td><button type="button" class="btn btn-default btn-md" data-toggle="modal" data-target="#visualizar"><i class="fa fa-eye" aria-hidden="true"></i></span> Visualizar Receita </button></td>';
+    
+                        $("#tableReceitas").append(row); //this will append tr element to table... keep its reference for a while since we will add cels into it
+                        row.append($("<td>" + valReceita.nome_receita + "</td>"));
+                        row.append($("<td>" + valClassificacao.descricao_classificacao + "</td>"));
+                        row.append($("<td>" + valCategoria.descricao_categoria + "</td>"))
+                        row.append(buttonEdit);
+                        row.append(buttonDelete);
+                        row.append(buttonView);   
+                    }
+                }
+            });
         });
     });
 }
@@ -61,8 +71,8 @@ function postJson() {
         $.each(jsonReceita, function (indexReceita, valReceita) {
             if (valReceita.id_receita == id) {
                 formArray.push({
-                    name: 'valor_ingrediente',
-                    value: '' + valIngrediente.valor_ingrediente + ''
+                    name: 'valor_receita',
+                    value: '' + valReceita.valor_receita + ''
                 }, {
                     name: 'quantidade_estoque_ingrediente',
                     value: '' + valIngrediente.quantidade_estoque_ingrediente + ''
